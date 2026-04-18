ce06773fdf5f9d682bf199bd8db93bd5dbbc11c0
jake22789 <70212338+jake22789@users.noreply.github.com>
2026-04-18 16:16:37 -0600

    fix

--- backend/obj/Debug/net9.0/final_proj.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/final_proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final_proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b4fda4174a092f9ccbf3bb556764cdcd9bfea0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea24171ab6b4c6e53322c934dda7851b09fbf80")]
 [assembly: System.Reflection.AssemblyProductAttribute("final_proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final_proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
